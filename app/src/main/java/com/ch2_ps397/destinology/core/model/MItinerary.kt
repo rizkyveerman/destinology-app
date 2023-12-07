3cc47960277361d59c91a3ecd91edd5f0f59e7b4
RIzky Firman Syah <rizfirsy@gmail.com>
2023-12-07 17:01:46 +0700

    [ADD] Setup API consumer methods

--- app/src/main/java/com/ch2_ps397/destinology/core/model/MItinerary.kt
+++ app/src/main/java/com/ch2_ps397/destinology/core/model/MItinerary.kt
@@ -1,4 +1,10 @@
 package com.ch2_ps397.destinology.core.model
 
-class MItinerary {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MItinerary (
+    var id: String = "",
+    var name: String = "",
+    var description: String = "",
+    var address: String = "",
+)
