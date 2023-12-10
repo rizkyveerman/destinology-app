5b8574e1634e05024cddcac89e9cf424f13c1654
RIzky Firman Syah <rizfirsy@gmail.com>
2023-12-10 14:57:40 +0700

    [ADD] Install the font

--- app/src/main/java/com/ch2_ps397/destinology/ui/theme/Type.kt
+++ app/src/main/java/com/ch2_ps397/destinology/ui/theme/Type.kt
@@ -4,32 +4,44 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.ch2_ps397.destinology.R
+
+val googleFontProvider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(
+            Font(GoogleFont("Poppins"), googleFontProvider)
+        ),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-        color = Black
+        letterSpacing = 0.sp,
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(
+            Font(GoogleFont("Poppins"), googleFontProvider)
+        ),
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
-        color = Black
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(
+            Font(GoogleFont("Poppins"), googleFontProvider)
+        ),
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp,
-        color = Black
     )
 )
