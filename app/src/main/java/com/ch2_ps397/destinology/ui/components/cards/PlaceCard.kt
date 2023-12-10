bc9f370c2a7ba1cfa04fc616af8f0a2641288d40
RIzky Firman Syah <rizfirsy@gmail.com>
2023-12-10 15:26:06 +0700

    [ADD] Place card UI

---
+++ app/src/main/java/com/ch2_ps397/destinology/ui/components/cards/PlaceCard.kt
@@ -0,0 +1,122 @@
+package com.ch2_ps397.destinology.ui.components.cards
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.FavoriteBorder
+import androidx.compose.material.icons.twotone.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.ch2_ps397.destinology.ui.theme.Black
+import com.ch2_ps397.destinology.ui.theme.Gray
+import com.ch2_ps397.destinology.ui.theme.Orange
+import com.ch2_ps397.destinology.ui.theme.VeryLightGray
+import com.ch2_ps397.destinology.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DestinologyPlaceCard(imageModel: String, title: String, rating: Float, onClick: () -> Unit) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        onClick = { onClick() },
+        modifier = Modifier.width(220.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = White,
+            contentColor = Black,
+        )
+    ) {
+        AsyncImage(
+            contentScale = ContentScale.Crop,
+            model = imageModel,
+            contentDescription = title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column {
+                Text(text = title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        imageVector = Icons.TwoTone.Star,
+                        contentDescription = "Rate",
+                        colorFilter = ColorFilter.tint(Orange),
+                        modifier = Modifier
+                            .size(22.dp)
+                            .padding(end = 4.dp)
+                        )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = rating.toString(), fontWeight = FontWeight.Bold, fontSize = 14.sp)
+                        Text(text = "(34 reviews)",  color = Gray, fontSize = 14.sp)
+                    }
+                }
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .width(32.dp)
+                    .height(32.dp)
+                    .clip(CircleShape)
+                    .background(VeryLightGray)
+                    .clickable {
+
+                    }
+            ) {
+                Image(
+                    imageVector = Icons.TwoTone.FavoriteBorder,
+                    contentDescription = "Like",
+                    colorFilter = ColorFilter.tint(Gray)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPlaceCard() {
+    DestinologyPlaceCard(
+        imageModel = "https://images.unsplash.com/photo-1556375413-f6cdc5e17398?q=80&w=2082&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title = "Candi Borobudur",
+        rating = 4.6f
+    ) {
+        
+    }
+}
